9fc12f164dde57b95066ac43128da7b95e5c6112
Dmitry <dslonkov@bk.ru>
2020-07-23 00:01:52 +0300

    another commit

--- script/script.js
+++ script/script.js
@@ -5,14 +5,14 @@ $(document).ready(function() {
     });
   $('.headingSearch').submit(function(e) {
     e.preventDefault();
-    $('.input1').css('display', 'block');
+    if ($(window).width() < 768) {
+      $('.input1').css('display', 'block');
+    }
     
   });
   $(document).click(function(e) { 
 		var input = $('.input1'); 
-		if (!input.is(e.target) && input.has(e.target).length === 0) { 		
+		if ((!input.is(e.target) && input.has(e.target).length === 0) && ($(window).width() < 768)) { 		
       input.hide(); 
-
   }});
-
 });
